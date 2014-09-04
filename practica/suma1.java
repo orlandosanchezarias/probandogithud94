c9b8edfb05c1fa614517402be1513912141b7152
Estudiantes <Estudiantes@S2dorada2-PC5>
2014-09-04 17:36:12 -0500

    actualizacion practica2

---
+++ practica/suma1.java
@@ -0,0 +1,10 @@
+
+public class suma1 {
+   public static void main(String[] args) {
+        int num=1;
+        while (num<=100) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
