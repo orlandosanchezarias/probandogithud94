a5299051b2828e643150b018681471c9883ccd75
Estudiantes <Estudiantes@S2dorada2-PC5>
2014-09-04 17:34:54 -0500

    actualizacion practica

---
+++ practica/multiplicar.java
@@ -0,0 +1,22 @@
+
+public class multiplicar {
+    public static void main(String [] args){
+
+int i,mul,t;
+
+for (t=1; t<=12;t++){
+
+System.out.println("-------------------------");
+
+System.out.println("La tabla del" +t);
+
+for (i=1; i<=12;i++){
+
+mul = i * t;
+
+System.out.println(t+" x "+i+" = "+mul);
+}
+System.out.println("---------------------");
+}
+}
+}
