f2f3f088e216d5954ef68ae026e93395dfe5f6f3
pmelo1981 <44075547+pmelo1981@users.noreply.github.com>
2020-05-15 09:32:14 -0500

    Update practica1.java

--- practica/practica1.java
+++ practica/practica1.java
@@ -3,5 +3,6 @@ public class practica1 {
     public static void main (String[]args){
         System.out.print("hola mundo");
         System.out.print("River Plate");
+        System.out.print("Hola Felipito");
     }
 }
